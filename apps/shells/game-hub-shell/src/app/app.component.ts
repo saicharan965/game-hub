5df8b0f7d35bb71970f49b5267308985437c89b3
Sai Charan <scharan@ariqt.com>
2024-12-17 19:04:14 +0530

    added game-hub-shell

---
+++ apps/shells/game-hub-shell/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'game-hub-root',
+  standalone: false,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'game-hub-shell';
+}
